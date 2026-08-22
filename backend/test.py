3a01749d36b063527ac33eea99ed62834031f200
Alakh-12 <mehtalakh07@gmail.com>
2026-08-23 04:23:57 +0530

    Add files via upload

--- backend/test.py
+++ backend/test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import requests
 
 response = requests.post('http://127.0.0.1:5000/analyze', json={
@@ -6,3 +7,13 @@ response = requests.post('http://127.0.0.1:5000/analyze', json={
 })
 
 print(response.json())
+=======
+import requests
+
+response = requests.post('http://127.0.0.1:5000/analyze', json={
+    "scenario": "Flood in Assam, 3 districts affected, 50000 displaced",
+    "disaster_type": "Flood"
+})
+
+print(response.json())
+>>>>>>> 9589ca86d52acbf1f3293bedbd7d4c11503eb453
